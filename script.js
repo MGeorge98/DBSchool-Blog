3099664a54ab8150ebae09314528a94fec673b48
Andrei Mateescu <andreimateescu120@yahoo.com>
2023-05-14 16:51:56 +0300

    Work in progress

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const body = document.getElementById('body');
-const main = document.getElementById('main');
+
+let container = document.createElement('div');
+container.className ='container';
+document.body.appendChild(container);
 
 //Navigation element
 const nav= document.createElement('nav');
 nav.className = "nav";
-document.body.appendChild(nav);
+container.appendChild(nav);
 
 //Navigation container
 const navContainer = document.createElement('ul');
@@ -59,7 +62,7 @@ navigationItem4.appendChild(navLink4);
 
 var buttonContainer = document.createElement('div');
 buttonContainer.className ='add__container';
-document.body.appendChild(buttonContainer);
+body.appendChild(buttonContainer);
 
 var addButton = document.createElement('button');
 addButton.innerText = ' + Add Article';
@@ -69,7 +72,113 @@ addButton.className = 'button';
 addButton.id= 'add';
 buttonContainer.appendChild(addButton);
 
-//
+//Get all articles
+
+// Fetch the articles list
+function getArticlesFromServer() {
+    fetch('http://localhost:3000/articles')
+        .then(function (response) {
+            // Trasform server response to plain object
+            response.json().then(function (articles) {
+                renderArticles(articles);
+            });
+        });
+};
+
+// Remove articles list if exist
+function removeOldArticlesFromDOM () {
+    while (body.firstChild) {
+        body.removeChild(body.firstChild);
+    }
+}
+
+function createArticleDOMNode(article) {
+
+    // Title
+    let title = document.createElement('h2');
+    title.className = "title";
+    title.textContent = article.title;
+
+    // Tag
+    let tag = document.createElement('li');
+    tag.className = "info__item";
+    tag.textContent = article.tag;
+
+    // Author
+    let author = document.createElement('span');
+    author.className = "info__mark";
+    author.textContent = article.author;
+
+    let authorContainer = document.createElement('li');
+    authorContainer.className = "info__item";
+    authorContainer.textContent = 'Added By ';
+    authorContainer.appendChild(author)
+
+    // Date
+    let date = document.createElement('li');
+    date.className = "info__item";
+    date.textContent = article.date;
+
+    // Information container
+    let infoContainer = document.createElement('ul');
+    infoContainer.className = "info__container";
+    infoContainer.appendChild(tag);
+    infoContainer.appendChild(authorContainer);
+    infoContainer.appendChild(date);
+
+    // Edit button
+    let editButton = document.createElement('button');
+    editButton.className = "actions__btn";
+    editButton.textContent = 'Edit';
+
+    // Delete button
+    let deleteButton = document.createElement('button');
+    deleteButton.className = "actions__btn";
+    deleteButton.textContent = 'Delete';
+
+    // Buttons container
+    let buttonsContainer = document.createElement('div');
+    buttonsContainer.className = "actions__container";
+    buttonsContainer.appendChild(editButton);
+    buttonsContainer.appendChild(deleteButton);
+
+    // Image
+    let img = document.createElement('img');
+    img.src = article.imgUrl;
+
+    // Paragraph
+    let paragraph = document.createElement('p');
+    paragraph.textContent = article.content;
+
+    // Paragraph container
+    let paragraphContainer = document.createElement('div');
+    paragraphContainer.className = "content__container";
+    paragraphContainer.appendChild(paragraph);
+
+    // Append all article nodes to container
+    let articleNode = document.createElement('article');
+    articleNode.appendChild(title);
+    articleNode.appendChild(infoContainer);
+    articleNode.appendChild(buttonsContainer);
+    articleNode.appendChild(img);
+    articleNode.appendChild(paragraphContainer);
+
+    return articleNode;
+}
+
+// Create DOM objects and append them to DOM
+function renderArticles(articles) {
+    
+    removeOldArticlesFromDOM();
+
+    // Create and append tags
+    for (let i = 0; i < articles.length; i++) {
+        let articleDOMNode = createArticleDOMNode(articles[i]);
+        body.appendChild(articleDOMNode);
+    }
+}
+
+getArticlesFromServer();
 
 // MODALA ADD / EDIT
 
