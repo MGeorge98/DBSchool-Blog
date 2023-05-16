b05100de42c05e52f3664a4ac4ee3e0d16cb394c
George Martinas <98george.martinas@gmail.com>
2023-05-16 11:10:37 +0300

    implemented add/edit/delete article

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 const body = document.getElementById('body');
 const main = document.getElementById('main')
 //Navigation element
-const nav= document.createElement('nav');
+const nav = document.createElement('nav');
 nav.className = "nav";
 main.appendChild(nav);
 //Add button
 var buttonContainer = document.createElement('div');
-buttonContainer.className ='add__container';
+buttonContainer.className = 'add__container';
 main.appendChild(buttonContainer);
 
 
 const container = document.createElement('div');
-container.className ='container';
+container.className = 'container';
 main.appendChild(container);
-
-
-
 
 //Navigation container
 const navContainer = document.createElement('ul');
@@ -28,7 +25,7 @@ navigationItem1.className = "nav__item";
 navContainer.appendChild(navigationItem1);
 
 const navLink1 = document.createElement('a');
-navLink1.className ="nav__link";
+navLink1.className = "nav__link";
 navLink1.href = "index.html";
 navLink1.textContent = "Travel updates";
 navigationItem1.appendChild(navLink1);
@@ -39,7 +36,7 @@ navigationItem2.className = "nav__item";
 navContainer.appendChild(navigationItem2);
 
 const navLink2 = document.createElement('a');
-navLink2.className ="nav__link";
+navLink2.className = "nav__link";
 navLink2.href = "reviews.html";
 navLink2.textContent = "Reviews";
 navigationItem2.appendChild(navLink2);
@@ -50,7 +47,7 @@ navigationItem3.className = "nav__item";
 navContainer.appendChild(navigationItem3);
 
 const navLink3 = document.createElement('a');
-navLink3.className ="nav__link";
+navLink3.className = "nav__link";
 navLink3.href = "about.html";
 navLink3.textContent = "About";
 navigationItem3.appendChild(navLink3);
@@ -61,7 +58,7 @@ navigationItem4.className = "nav__item";
 navContainer.appendChild(navigationItem4);
 
 const navLink4 = document.createElement('a');
-navLink4.className ="nav__link";
+navLink4.className = "nav__link";
 navLink4.href = "contact.html";
 navLink4.textContent = "Contact";
 navigationItem4.appendChild(navLink4);
@@ -75,8 +72,56 @@ addButton.innerText = ' + Add Article';
 // addButton.onclick = openAddModal;
 addButton.addEventListener('click', openAddModal);
 addButton.className = 'button';
-addButton.id= 'add';
+addButton.id = 'add';
 buttonContainer.appendChild(addButton);
+
+// Add article
+function addArticleToServer() {
+    console.log("test")
+    let formTitle = document.getElementById('formTitle');
+    let formTag = document.getElementById('formTag');
+    let formAuthor = document.getElementById('formAuthor');
+    let formDate = document.getElementById('formDate');
+    let formImgUrl = document.getElementById('formImgUrl');
+    let formContent = document.getElementById('formContent');
+    // creat post object
+    const postObject = {
+        title: formTitle.value,
+        tag: formTag.value,
+        author: formAuthor.value,
+        date: formDate.value,
+        imgUrl: formImgUrl.value,
+        content: formContent.value
+    }
+    // Call post request to add a new article
+    fetch('http://localhost:3000/articles', {
+        method: 'post',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(postObject)
+    }).then(function () {
+        console.log("test")
+        // Get the new article list
+        getArticlesFromServer();
+
+        // Reset Form
+        resetForm();
+
+        // Close Modal
+        closeModal();
+    });
+}
+
+// Reset form values
+function resetForm() {
+    formTitle.value = '';
+    formTag.value = '';
+    formAuthor.value = '';
+    formDate.value = '';
+    formImgUrl.value = '';
+    formContent.value = '';
+}
 
 //Get all articles
 
@@ -92,7 +137,7 @@ function getArticlesFromServer() {
 };
 
 // Remove articles list if exist
-function removeOldArticlesFromDOM () {
+function removeOldArticlesFromDOM() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
@@ -135,11 +180,18 @@ function createArticleDOMNode(article) {
     // Edit button
     let editButton = document.createElement('button');
     editButton.className = "actions__btn";
+    editButton.addEventListener('click', function () {
+        openEditModal(article);
+    })
     editButton.textContent = 'Edit';
 
     // Delete button
     let deleteButton = document.createElement('button');
     deleteButton.className = "actions__btn";
+    // Add event on delete button and pass article id to delete it form server more at https://stackoverflow.com/questions/256754/how-to-pass-arguments-to-addeventlistener-listener-function
+    deleteButton.addEventListener('click', function () {
+        deleteArticleFromServer(article.id);
+    });
     deleteButton.textContent = 'Delete';
 
     // Buttons container
@@ -151,6 +203,7 @@ function createArticleDOMNode(article) {
     // Image
     let img = document.createElement('img');
     img.src = article.imgUrl;
+    img.className = "article__img";
 
     // Paragraph
     let paragraph = document.createElement('p');
@@ -163,6 +216,7 @@ function createArticleDOMNode(article) {
 
     // Append all article nodes to container
     let articleNode = document.createElement('article');
+    articleNode.className = "article__container";
     articleNode.appendChild(title);
     articleNode.appendChild(infoContainer);
     articleNode.appendChild(buttonsContainer);
@@ -172,9 +226,52 @@ function createArticleDOMNode(article) {
     return articleNode;
 }
 
+// Delete article from server
+function deleteArticleFromServer(id) {
+    // Call delete request to delete the article
+    fetch(`http://localhost:3000/articles/${id}`, {
+        method: 'DELETE',
+    }).then(function () {
+        // Get the new articles list
+        getArticlesFromServer();
+    });
+}
+
+// Update article
+function updateArticleToServer(id) {
+    // creat put object
+    const putObject = {
+        title: formTitle.value,
+        tag: formTag.value,
+        author: formAuthor.value,
+        date: formDate.value,
+        imgUrl: formImgUrl.value,
+        saying: formSaying.value,
+        summary: formSummary.value,
+        content: formContent.value
+    }
+    // Call put request to update the article
+    fetch(`http://localhost:3000/articles/${id}`, {
+        method: 'PUT',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(putObject)
+    }).then(function () {
+        // Get the new article list
+        getArticlesFromServer();
+
+        // Reset Form
+        resetForm();
+
+        // Close Modal
+        closeModal();
+    });
+}
+
 // Create DOM objects and append them to DOM
 function renderArticles(articles) {
-    
+
     removeOldArticlesFromDOM();
 
     // Create and append tags
@@ -189,13 +286,13 @@ getArticlesFromServer();
 // MODALA ADD / EDIT
 
 var modalOverlay = document.createElement('div');
-modalOverlay.className ='modal__overlay';
+modalOverlay.className = 'modal__overlay';
 body.appendChild(modalOverlay);
 var modal = document.createElement('div');
-modal.className ='modal';
+modal.className = 'modal';
 modalOverlay.appendChild(modal);
 var modalContent = document.createElement('div');
-modalContent.className ='modal__content';
+modalContent.className = 'modal__content';
 modal.appendChild(modalContent);
 
 // Create a <h2> element
@@ -218,6 +315,8 @@ for (var i = 0; i < inputIds.length; i++) {
     input.placeholder = inputPlaceholders[i];
     inputsContainer.appendChild(input);
 }
+
+
 
 // Create a textarea
 var textarea = document.createElement('textarea');
@@ -262,11 +361,32 @@ modal.appendChild(buttonsContainer);
 
 
 function openAddModal() {
+    let saveButton = document.getElementById('save');
+    saveButton.addEventListener('click', function () {
+        addArticleToServer()
+    });
+    body.className = 'show-modal';
+}
 
-    // saveButton.addEventListener('click', function () {
-    //     addArticleToServer()
-    // });
+function openEditModal(article) {
+    let formTitle = document.getElementById('formTitle');
+    let formTag = document.getElementById('formTag');
+    let formAuthor = document.getElementById('formAuthor');
+    let formDate = document.getElementById('formDate');
+    let formImgUrl = document.getElementById('formImgUrl');
+    let formContent = document.getElementById('formContent');
+    // Copy article information to form
+    formTitle.value = article.title;
+    formTag.value = article.tag;
+    formAuthor.value = article.author;
+    formDate.value = article.date;
+    formImgUrl.value = article.imgUrl;
+    formContent.value = article.content;
 
+    let saveButton = document.getElementById('save');
+    saveButton.addEventListener('click', function () {
+        updateArticleToServer(article.id)
+    });
     body.className = 'show-modal';
 }
 
@@ -282,13 +402,13 @@ footer.className = "footer";
 document.body.appendChild(footer);
 
 const footerLink1 = document.createElement('a');
-footerLink1.className ="footer__link";
+footerLink1.className = "footer__link";
 footerLink1.href = "";//to decide later what to connect
 footerLink1.textContent = "previous";
 footer.appendChild(footerLink1);
 
 const footerLink2 = document.createElement('a');
-footerLink2.className ="footer__link";
+footerLink2.className = "footer__link";
 footerLink2.href = "";//to decide later what to connect
 footerLink2.textContent = "next";
 footer.appendChild(footerLink2);
